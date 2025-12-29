43252258751722f3e64f45dbcb802758e1a4f059
Luis Castro <luisr.contato@gmail.com>
2025-12-29 09:46:29 -0300

    Upload projeto

---
+++ app.js
@@ -0,0 +1,99 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados)
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
+
+/*
+
+                        Comandos Git
+
+git init: Inicia um novo repositório Git local.
+git add [arquivo]: Adiciona um arquivo específico ao estágio
+para o próximo commit.
+git add .: Adiciona todos os arquivos modificados ao estágio para
+o próximo commit.
+git commit -m "mensagem": Cria um commit com uma mensagem
+descritiva.
+git remote add origin [URL]: Adiciona um repositório remoto chamado
+"origin".
+git push -u origin main: Envia os commits locais para o branch
+"main" do repositório remoto.
+
+git config --global user.email "luisr.contato@gmail.com":
+Configura o email do usuário
+git config --global user.name "Luis Castro": Configura o nome de
+usuário
+
+                        Conceitos
+
+Deploy: O processo de colocar uma aplicação ou site
+disponível para acesso público na internet.
+
+Repositório local: Um diretório no seu computador onde o Git
+armazena o histórico de versões do seu projeto.
+
+Repositório remoto: Um repositório hospedado em um servidor
+remoto, como o GitHub, que permite colaboração e backup do código.
+*/
